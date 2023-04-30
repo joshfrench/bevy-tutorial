6672ce9389b20f279866b0f949556d18c829bdd5
Josh French <joshfrench@gmail.com>
2023-04-30 12:01:12 -0400

    movement

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+use rand::prelude::*;
+
+pub const PLAYER_SIZE: f32 = 64.;
+pub const PLAYER_SPEED: f32 = 500.;
+pub const NUMBER_OF_ENEMIES: usize = 4;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_enemies)
+        .add_system(player_movement)
+        .add_system(confine_player_movement)
         .run();
 }
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct Enemy;
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -35,4 +46,85 @@ pub fn spawn_camera(mut commands: Commands, q: Query<&Window, With<PrimaryWindow
         transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
         ..default()
     });
+}
+
+pub fn spawn_enemies(
+    mut commands: Commands,
+    wq: Query<&Window, With<PrimaryWindow>>,
+    assets: Res<AssetServer>,
+) {
+    let window = wq.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let rand_x = random::<f32>() * window.width();
+        let rand_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(rand_x, rand_y, 0.),
+                texture: assets.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
+    }
+}
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut q: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = q.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1., 0., 0.);
+        }
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1., 0., 0.);
+        }
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0., 1., 0.);
+        }
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(0., -1., 0.);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut pq: Query<&mut Transform, With<Player>>,
+    wq: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut p_xform) = pq.get_single_mut() {
+        let window = wq.get_single().unwrap();
+        let half_player_size = PLAYER_SIZE / 2.;
+        let x_min = 0. + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0. + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = p_xform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        p_xform.translation = translation;
+    }
 }
